b1a43bcfb46791dea2eea2b0b2039a2e056b1726
Brian Scaturro <scaturrob@gmail.com>
2014-11-15 14:51:41 -0500

    include @property docs for Scope

--- src/Scope.php
+++ src/Scope.php
@@ -3,8 +3,32 @@ namespace Peridot\Leo;
 
 use Peridot\Scope\Scope as PeridotScope;
 
+/**
+ * The Leo Scope contains the chainable interface
+ * and is the main entry point for making assertions.
+ *
+ * @package Peridot\Leo
+ *
+ * @property Scope to
+ * @property Scope be
+ * @property Scope been
+ * @property Scope is
+ * @property Scope that
+ * @property Scope and
+ * @property Scope has
+ * @property Scope have
+ * @property Scope with
+ * @property Scope at
+ * @property Scope of
+ * @property Scope same
+ */
 class Scope extends PeridotScope
 {
+    /**
+     * Specifies the chainable interface for assertions
+     *
+     * @var array
+     */
     protected $chainables = [
         'to',
         'be',
