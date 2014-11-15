40bd74effd778cd88e02d6d10065206d6ebc630f
Brian Scaturro <scaturrob@gmail.com>
2014-11-15 15:02:25 -0500

    subject delegates __get calls to scope

--- specs/subject.spec.php
+++ specs/subject.spec.php
@@ -2,16 +2,23 @@
 use Peridot\Leo\Subject;
 
 describe('Subject', function () {
+
+    beforeEach(function() {
+        $this->subject = new Subject("actual");
+    });
+
     describe('actual value', function () {
-
-        beforeEach(function() {
-            $this->subject = new Subject("actual");
-        });
-
         it('should be accessible', function () {
             $this->subject->setActual("hello");
             $actual = $this->subject->getActual();
             assert($actual == "hello", "expected 'hello', got '$actual'");
+        });
+    });
+
+    describe('->__get()', function() {
+        it("should delegate to scope", function() {
+            $scope = $this->subject->to;
+            assert($scope === $this->subject->getScope(), "should delegate __get to scope");
         });
     });
 });
