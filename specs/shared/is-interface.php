60c134e4a9389112b27182803090b3595574c7a6
Brian Scaturro <scaturrob@gmail.com>
2014-11-27 13:01:13 -0500

    starting over

--- specs/shared/is-interface.php
+++
@@ -1,8 +0,0 @@
-<?php
-describe('assertion subject', function () {
-    it('should be accessible', function () {
-        $this->interface->setSubject("hello");
-        $subject = $this->interface->getSubject();
-        assert($subject == "hello", "expected 'hello', got '$subject'");
-    });
-});
