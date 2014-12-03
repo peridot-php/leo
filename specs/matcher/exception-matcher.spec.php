f90c9db78b6631d26b6b547d0f13ca8f13c7983b
Brian Scaturro <scaturrob@gmail.com>
2014-12-02 20:38:40 -0500

    fix exception matcher tests

--- specs/matcher/exception-matcher.spec.php
+++ specs/matcher/exception-matcher.spec.php
@@ -2,47 +2,51 @@
 use Peridot\Leo\Matcher\ExceptionMatcher;
 use Peridot\Leo\Matcher\Template\ArrayTemplate;
 
-xdescribe('ExceptionMatcher', function() {
+describe('ExceptionMatcher', function() {
 
     beforeEach(function() {
-        $this->matcher = new ExceptionMatcher(function() {
-            throw new DomainException("hello world");
-        });
+        $this->matcher = new ExceptionMatcher('DomainException');
     });
 
     describe('->match()', function() {
         it('should return true result if callable throws exception', function() {
-            $result = $this->matcher->match('DomainException');
+            $result = $this->matcher->match(function() {
+                throw new DomainException('hello world');
+            });
             expect($result->isMatch())->to->equal(true);
         });
 
         it('should return false result if callable throws different type', function() {
-            $result = $this->matcher->match('RuntimeException');
+            $result = $this->matcher->match(function() {
+                throw new RuntimeException('hello world');
+            });
             expect($result->isMatch())->to->equal(false);
         });
 
         it('should throw exception if callable is not valid', function() {
             $obj = new stdClass();
-            $matcher = new ExceptionMatcher([$obj, 'nope']);
-            expect([$matcher, 'match'])->with('DomainException')->to->throw('BadFunctionCallException');
+            $matcher = new ExceptionMatcher('DomainException');
+            expect([$matcher, 'match'])->with([$obj, 'nope'])->to->throw('BadFunctionCallException');
         });
 
         it('should return false when function throws no exception', function() {
-            $matcher = new ExceptionMatcher(function() {
-
-            });
-            $result = $matcher->match('Exception');
+            $matcher = new ExceptionMatcher('Exception');
+            $result = $matcher->match(function() {});
             expect($result->isMatch())->to->equal(false);
         });
 
         context('when inverted', function() {
             it('should return true result if exceptions are different', function() {
-                $result = $this->matcher->invert()->match('RuntimeException');
+                $result = $this->matcher->invert()->match(function() {
+                    throw new RuntimeException();
+                });
                 expect($result->isMatch())->to->equal(true);
             });
 
             it('should return false result if exceptions are same', function() {
-                $result = $this->matcher->invert()->match('DomainException');
+                $result = $this->matcher->invert()->match(function() {
+                    throw new DomainException();
+                });
                 expect($result->isMatch())->to->equal(false);
             });
         });
@@ -50,26 +54,34 @@ xdescribe('ExceptionMatcher', function() {
         context('when matching exception message', function() {
             it('should return true result if messages are the same', function() {
                 $this->matcher->setExpectedMessage("hello world");
-                $result = $this->matcher->match('Exception');
+                $result = $this->matcher->match(function() {
+                    throw new DomainException('hello world');
+                });
                 expect($result->isMatch())->to->equal(true);
             });
 
             it('should return false result if messages are different', function() {
                 $this->matcher->setExpectedMessage("goodbye");
-                $result = $this->matcher->match('Exception');
+                $result = $this->matcher->match(function() {
+                    throw new DomainException('hello world');
+                });
                 expect($result->isMatch())->to->equal(false);
             });
 
             context('and matcher is inverted', function() {
                 it('should return true result if exception messages are different', function() {
                     $this->matcher->setExpectedMessage('goodbye');
-                    $result = $this->matcher->invert()->match('DomainException');
+                    $result = $this->matcher->invert()->match(function() {
+                        throw new DomainException('hello world');
+                    });
                     expect($result->isMatch())->to->equal(true);
                 });
 
                 it('should return false result if exception messages are same', function() {
                     $this->matcher->setExpectedMessage('hello world');
-                    $result = $this->matcher->invert()->match('DomainException');
+                    $result = $this->matcher->invert()->match(function() {
+                        throw new DomainException('hello world');
+                    });
                     expect($result->isMatch())->to->equal(false);
                 });
             });
