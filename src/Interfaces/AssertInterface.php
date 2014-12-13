22b2c43dbca78115d3f23ea18f3e002b3c9862d6
Brian Scaturro <scaturrob@gmail.com>
2014-12-13 18:54:37 -0500

    remove DynamicObjectTrait from AssertInterface

--- src/Interfaces/AssertInterface.php
+++ src/Interfaces/AssertInterface.php
@@ -2,18 +2,17 @@
 namespace Peridot\Leo\Interfaces;
 
 use Peridot\Leo\Assertion;
-use Peridot\Leo\DynamicObjectTrait;
 use Peridot\Leo\Leo;
 use Peridot\Leo\Responder\ResponderInterface;
 
 /**
- * Class AssertInterface
+ * AssertInterface is a non-chainable, object oriented interface
+ * on top of a Leo Assertion.
+ *
  * @package Peridot\Leo\Interfaces
  */
 class AssertInterface
 {
-    use DynamicObjectTrait;
-
     /**
      * @var Assertion
      */
@@ -28,16 +27,85 @@ class AssertInterface
             $assertion = Leo::instance()->getAssertion();
         }
         $this->assertion = $assertion;
-        include __DIR__ . '/interface.assert.php';
     }
 
     /**
-     * @param $method
-     * @param $args
-     * @return mixed
+     * Perform an a loose equality assertion.
+     *
+     * @param $actual
+     * @param $expected
+     * @param string $message
      */
-    public function __call($method, $args)
+    public function equal($actual, $expected, $message = "")
     {
-        return call_user_func_array($this->methods[$method], $args);
+        $this->assertion->setActual($actual);
+        $this->assertion->to->loosely->equal($expected, $message);
+    }
+
+    /**
+     * Perform a negated loose equality assertion.
+     *
+     * @param $actual
+     * @param $expected
+     * @param string $message
+     */
+    public function notEqual($actual, $expected, $message = "")
+    {
+        $this->assertion->setActual($actual);
+        $this->assertion->to->not->equal($expected, $message);
+    }
+
+    /**
+     * Performs a throw assertion.
+     *
+     * @param callable $fn
+     * @param $exceptionType
+     * @param string $exceptionMessage
+     * @param string $message
+     */
+    public function throws(callable $fn, $exceptionType, $exceptionMessage = "", $message = "")
+    {
+        $this->assertion->setActual($fn);
+        $this->assertion->to->throw($exceptionType, $exceptionMessage, $message);
+    }
+
+    /**
+     * Performs a negated throw assertion.
+     *
+     * @param callable $fn
+     * @param $exceptionType
+     * @param string $exceptionMessage
+     * @param string $message
+     */
+    public function doesNotThrow(callable $fn, $exceptionType, $exceptionMessage = "", $message = "")
+    {
+        $this->assertion->setActual($fn);
+        $this->assertion->not->to->throw($exceptionType, $exceptionMessage, $message);
+    }
+
+    /**
+     * Performs a type assertion.
+     *
+     * @param $actual
+     * @param $expected
+     * @param string $message
+     */
+    public function typeOf($actual, $expected, $message = "")
+    {
+        $this->assertion->setActual($actual);
+        $this->assertion->to->be->a($expected, $message);
+    }
+
+    /**
+     * Performs a negated type assertion.
+     *
+     * @param $actual
+     * @param $expected
+     * @param string $message
+     */
+    public function notTypeOf($actual, $expected, $message = "")
+    {
+        $this->assertion->setActual($actual);
+        $this->assertion->to->not->be->a($expected, $message);
     }
 }
